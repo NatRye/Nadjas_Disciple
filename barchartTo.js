23ef72505bb42888762d4d61d9414d8d61663462
noahrdam <noahrdam@gmail.com>
2023-11-29 16:17:00 +0100

    grid css barchart 2 og rettelser

--- barchartTo.js
+++ barchartTo.js
@@ -1,71 +1,76 @@
-//Barchart 1
 // set the dimensions and margins of the graph
-const margin = { top: 10, right: 30, bottom: 90, left: 60 },
-  bredde = 600 - margin.left - margin.right,
-  højde = 500 - margin.top - margin.bottom;
+const margin2 = { top: 10, right: 30, bottom: 90, left: 60 };
+const bredde2 = 600 - margin2.left - margin2.right;
+const højde2 = 500 - margin2.top - margin2.bottom;
 
 // append the svg object to the body of the page
-const svg1 = d3
-  .select("#barchart1")
+const svg2 = d3
+  .select("#barchart2")
   .append("svg")
-  .attr("width", bredde + margin.left + margin.right + 700) // Increased width for text
-  .attr("height", højde + margin.top + margin.bottom)
+  .attr("width", bredde2 + margin2.left + margin2.right) // Increased width for text
+  .attr("height", højde2 + margin2.top + margin2.bottom)
   .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
+  .attr("transform", `translate(${margin2.left},${margin2.top})`);
 
 // dataset(data.data)
-fetch("http://localhost:3000/barchart1")
+fetch("http://localhost:3000/barchart2")
   .then((response) => response.json())
   .then((result) => {
     const data = result.data; // Access the data property
 
-    svg1
+    svg2
       .append("text")
-      .attr("x", bredde + margin.right - 30) // Adjusted x position
-      .attr("y", højde / 6) // Adjusted y position
+      .attr("x", bredde2 + margin2.right - 30) // Adjusted x position
+      .attr("y", højde2 / 6) // Adjusted y position
       .attr("transform", "rotate(0)")
       .style("text-anchor", "start") // Changed text-anchor to "start"
       .style("font-size", "20px") // Changed font size
       .style(
         "font-family",
         "Roobert,-apple-system,BlinkMacSystemFont,Helvetica,Arial,sans-serif"
-      );
+      )
+      .selectAll("tspan")
+      .enter()
+      .append("tspan")
+      .attr("x", bredde2 + margin2.right - 30) // Same x position for each line
+      .attr("dy", "1em") // Adjust vertical spacing between lines
+      .text((d) => d);
 
     // X axis
     const x = d3
       .scaleBand()
-      .range([0, bredde])
+      .range([0, bredde2])
       .domain(data.map((d) => d.entity))
       .padding(0.2);
-    svg1
+    svg2
       .append("g")
-      .attr("transform", `translate(0,${højde})`)
+      .attr("transform", `translate(0,${højde2})`)
       .call(d3.axisBottom(x))
       .selectAll("text")
       .attr("transform", "translate(-10,0)rotate(-45)")
       .style("text-anchor", "end");
 
     // Y axis label
-    svg1
+    svg2
       .append("text")
       .attr("text-anchor", "center")
-      .attr("y", -45)
+      .attr("y", -46)
       .attr("x", -250)
       .style("font-size", "20px")
       .attr("transform", "rotate(-90)")
-      .text("Kg. per year");
+      .text("Tons per year");
 
     // Add Y axis
     const y = d3
       .scaleLinear()
       .domain([0, d3.max(data, (d) => +d.mismanaged)])
-      .range([højde, 0]);
-    svg1.append("g").call(d3.axisLeft(y));
+      .range([højde2, 0]);
+    svg2.append("g").call(d3.axisLeft(y));
 
     //Når musen holdes over et land udføres
     let mouseOverBar = function (d) {
       // Tooltip
-      const tooltip = svg1
+      const tooltip = svg2
         .append("text")
         .attr("class", "tooltip")
         .text(d.mismanaged)
@@ -83,7 +88,7 @@ fetch("http://localhost:3000/barchart1")
     // Hide value on mouseleave
     let mouseLeaveBar = function () {
       // Remove tooltip
-      svg1.selectAll(".tooltip").remove();
+      svg2.selectAll(".tooltip").remove();
 
       // Unhighlight the bar
       d3.select(this).style("opacity", 1).style("stroke", "none");
@@ -93,10 +98,10 @@ fetch("http://localhost:3000/barchart1")
     const colorScale = d3
       .scaleLinear()
       .domain([0, d3.max(data, (d) => +d.mismanaged)])
-      .range(["orange", "red"]); // Adjust the range of colors as needed
+      .range(["lightblue", "darkblue"]); // Adjust the range of colors as needed
 
     // Bars
-    svg1
+    svg2
       .selectAll("rect")
       .data(data)
       .enter()
@@ -110,7 +115,7 @@ fetch("http://localhost:3000/barchart1")
       .on("mouseleave", mouseLeaveBar);
 
     // Animation
-    svg1
+    svg2
       .selectAll("rect")
       .transition()
       .duration(800)
