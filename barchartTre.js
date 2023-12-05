77b2ddf98a9883b6eda641dee6f327297ee1c7ba
jabir8210 <jabir8210@gmail.com>
2023-12-05 09:41:55 +0100

    barchart 3 næsten færdig

--- barchartTre.js
+++ barchartTre.js
@@ -1,13 +1,17 @@
-// bar chart 3
+//Barchart 1
+// sætter dimensioner til visualiseringen
+const margin3 = { top: 10, right: 30, bottom: 90, left: 60 };
+const bredde3 = 600 - margin3.left - margin3.right;
+const højde3 = 500 - margin3.top - margin3.bottom;
 
 // appender svg objektet til body med select på id
 const svg3 = d3
   .select("#barchart3")
   .append("svg")
-  .attr("width", bredde + margin.left + margin.right + 700) // Increased width for text
-  .attr("height", højde + margin.top + margin.bottom)
+  .attr("width", bredde3 + margin3.left + margin3.right)
+  .attr("height", højde3 + margin3.top + margin3.bottom)
   .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
+  .attr("transform", `translate(${margin3.left},${margin3.top})`);
 
 // dataset(data.data)
 fetch("http://localhost:3000/barchart3")
@@ -15,41 +19,36 @@ fetch("http://localhost:3000/barchart3")
   .then((result) => {
     const data = result.data; // tilgår data property
 
-    // Y akse
-    const y = d3
+    // X akse
+    const x = d3
       .scaleBand()
-      .range([0, højde])
+      .range([0, bredde3])
       .domain(data.map((d) => d.entity))
       .padding(0.2);
     svg3
       .append("g")
-      .call(d3.axisLeft(y))
-      .selectAll("text")
-      .attr("transform", "translate(-5,0)")
-      .style("text-anchor", "end");
-
-    // X akse label
-    svg3
-      .append("text")
-      .attr("text-anchor", "center")
-      .attr("y", højde + margin.bottom - 100)
-      .attr("x", bredde / 3)
-      .style("font-size", "20px")
-      .text("Tons per year");
-
-    // X akse
-    const x = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => +d.mismanaged)])
-      .range([0, bredde]);
-
-    svg3
-      .append("g")
-      .attr("transform", `translate(0, ${højde})`)
+      .attr("transform", `translate(0,${højde})`)
       .call(d3.axisBottom(x))
       .selectAll("text")
       .attr("transform", "translate(-10,0)rotate(-45)")
       .style("text-anchor", "end");
+
+    // Y akse labels
+    svg3
+      .append("text")
+      .attr("text-anchor", "center")
+      .attr("y", -45)
+      .attr("x", -250)
+      .style("font-size", "20px")
+      .attr("transform", "rotate(-90)")
+      .text("Tons per year");
+
+    // Y akse
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => +d.mismanaged)])
+      .range([højde3, 0]);
+    svg3.append("g").call(d3.axisLeft(y));
 
     //Når musen holdes over et land udføres
     let mouseOverBar = function (d) {
@@ -58,22 +57,23 @@ fetch("http://localhost:3000/barchart3")
         .append("text")
         .attr("class", "tooltip")
         .text(d.mismanaged)
-        .attr("x", x(d.entity) + y.bandwidth() / 2)
+        .attr("x", x(d.entity) + x.bandwidth() / 2)
         .attr("y", y(d.mismanaged) - 2)
+        .attr("text-anchor", "middle")
         .attr("font-family", "sans-serif")
         .attr("font-size", "12px")
         .attr("fill", "black");
 
-      // Highlight på bar
+      // Highlight på baren
       d3.select(this).style("opacity", 0.25).style("stroke", "black");
     };
 
-    // gem value når mouseleave
+    // Hgem value når mouseleave
     let mouseLeaveBar = function () {
       // fjern tooltip
       svg3.selectAll(".tooltip").remove();
 
-      // Unhighlight bar
+      // Unhighlight  baren
       d3.select(this).style("opacity", 1).style("stroke", "none");
     };
 
@@ -81,7 +81,7 @@ fetch("http://localhost:3000/barchart3")
     const colorScale = d3
       .scaleLinear()
       .domain([0, d3.max(data, (d) => +d.mismanaged)])
-      .range(["#FFAB7E", "#C9372C"]); // Adjust the range of colors as needed
+      .range(["#F2D6A2", "#D96E48"]); // Adjust the range of colors as needed
 
     // Bars
     svg3
@@ -89,11 +89,11 @@ fetch("http://localhost:3000/barchart3")
       .data(data)
       .enter()
       .append("rect")
-      .attr("y", (d) => y(d.entity))
-      .attr("height", y.bandwidth())
+      .attr("x", (d) => x(d.entity))
+      .attr("width", x.bandwidth())
       .attr("fill", (d) => colorScale(d.mismanaged))
-      .attr("x", 0)
-      .attr("width", (d) => x(d.mismanaged))
+      .attr("y", (d) => y(0))
+      .attr("height", 0)
       .on("mouseover", mouseOverBar)
       .on("mouseleave", mouseLeaveBar);
 
@@ -102,8 +102,8 @@ fetch("http://localhost:3000/barchart3")
       .selectAll("rect")
       .transition()
       .duration(800)
-      .attr("x", (d) => y(d.mismanaged))
-      .attr("bredde", (d) => bredde - x(d.mismanaged))
+      .attr("y", (d) => y(d.mismanaged))
+      .attr("height", (d) => højde3 - y(d.mismanaged))
       .delay((d, i) => i * 100);
   })
   .catch((error) => console.error("Error fetching data:", error));
